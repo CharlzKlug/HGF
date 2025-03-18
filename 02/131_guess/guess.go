fe84dcbcd522eb0a192e79962c9facf0fd84a21a
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-03-18 16:08:06 +0300

    Using a loop in our guessing game

--- 02/131_guess/guess.go
+++ 02/131_guess/guess.go
@@ -21,21 +21,24 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Make a guess: ")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input= strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10 - guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input= strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
 
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW.")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH.")
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		}
 	}
 }
