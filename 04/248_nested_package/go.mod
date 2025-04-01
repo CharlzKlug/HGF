c39789ec641c7fefaa367a93b59aaad086429ab7
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-01 23:32:50 +0300

    Nested Package

---
+++ 04/248_nested_package/go.mod
@@ -0,0 +1,3 @@
+module 248_nested_package
+
+go 1.24.1
---
+++ 04/248_nested_package/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"248_nested_package/greeting"
+	"248_nested_package/greeting/deutsch"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+	deutsch.Hallo()
+	deutsch.GutenTag()
+}
