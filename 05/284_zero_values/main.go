eab2104d43b750bd0addb722f3481b502d414a53
Imil Ametov <charlz_klug@mail.ru>
2025-04-02 17:06:03 +0300

    Arrays Literals

--- 05/284_zero_values/main.go
+++ 05/284_zero_values/main.go
@@ -19,4 +19,15 @@ func main() {
 	counters[0]++
 	counters[2]++
 	fmt.Println(counters[0], counters[1], counters[2])
+
+	var bars [7]string = [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
+	fmt.Println(bars[3], bars[6], bars[0])
+	var somePrimes [5] int = [5]int{2, 3, 5, 7, 11}
+	fmt.Println(somePrimes[0], somePrimes[2], somePrimes[4])
+
+	text := [3]string {"This is a series of long strings",
+		"which would be awkward to place",
+		"together on a single line",
+	}
+	fmt.Println(text[0])
 }
