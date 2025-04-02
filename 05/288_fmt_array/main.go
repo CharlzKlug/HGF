1f4293376b195779d0e897b78ececff94bdeb5c7
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-02 23:50:39 +0300

    fmt Arrays

---
+++ 05/288_fmt_array/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notes [3]string = [3]string{"do", "re", "mi"}
+	var primes [5]int = [5]int{2, 3, 5, 7, 11}
+	fmt.Println(notes)
+	fmt.Println(primes)
+}
