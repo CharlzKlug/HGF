5e46a9edc953a27e5477c63cf3d8d9bda3104264
Imil Ametov <charlz_klug@mail.ru>
2025-04-02 16:46:54 +0300

    Arrays Initialize

---
+++ 05/284_zero_values/main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+func main() {
+	var primes [5]int
+	primes[0] = 2
+	fmt.Println(primes[0])
+	fmt.Println(primes[2])
+	fmt.Println(primes[4])
+	// fmt.Println(primes[5])
+
+	var notes [7]string
+	notes[0] = "do"
+	fmt.Println(notes[3])
+	fmt.Println(notes[6])
+	fmt.Println(notes[0])
+
+	var counters [3]int
+	counters[0]++
+	counters[0]++
+	counters[2]++
+	fmt.Println(counters[0], counters[1], counters[2])
+}
