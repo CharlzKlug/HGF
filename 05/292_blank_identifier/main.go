d8076e3b026a2c5fc5143a0c4083aa8f24fadf99
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-04 00:36:25 +0300

    Blank Identifier

---
+++ 05/292_blank_identifier/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
+
+for _, note := range notes {
+	fmt.Println(note)
+	// fmt.Println(index)
+}
+}
