da57e685bf3beba3feb2bea26e491d9a7add6889
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-04 00:48:24 +0300

    Formatted 'Blank Identifier'

--- 05/292_blank_identifier/main.go
+++ 05/292_blank_identifier/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
 
-for _, note := range notes {
-	fmt.Println(note)
-	// fmt.Println(index)
-}
+	for _, note := range notes {
+		fmt.Println(note)
+		// fmt.Println(index)
+	}
 }
