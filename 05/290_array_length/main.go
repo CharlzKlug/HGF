cfb9cb5bb955d2806a1c60af8e4553f5c89f3155
Imil Ametov <charlz_klug@mail.ru>
2025-04-03 11:07:30 +0300

    Array Length

---
+++ 05/290_array_length/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
+	fmt.Println(len(notes))
+	primes := [5]int{2, 3, 5, 7, 11}
+	fmt.Println(len(primes))
+
+	for i := 0; i < len(notes); i++ {
+		fmt.Println(i, notes[i])
+	}
+}
