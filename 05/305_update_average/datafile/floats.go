9912b19b9c73ed18b3a396b93ab8cb444d83f7f8
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-09 00:21:58 +0300

    Average changed to slice

--- 05/305_update_average/datafile/floats.go
+++ 05/305_update_average/datafile/floats.go
@@ -8,8 +8,8 @@ import (
 )
 
 // GetFloats reads a float64 from each line of a file.
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
@@ -17,11 +17,12 @@ func GetFloats(fileName string) ([3]float64, error) {
 	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		numbers, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		// i++
+		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
