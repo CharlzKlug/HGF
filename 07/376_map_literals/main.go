fc6340476cf45faa049351162532b7a7c0623a4a
Imil Ametov <charlz_klug@mail.ru>
2025-04-14 10:17:28 +0300

    Map Literals

---
+++ 07/376_map_literals/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	ranks := map[string]int{"bronze": 3, "silver": 2, "gold": 1}
+	fmt.Println(ranks["gold"])
+	fmt.Println(ranks["bronze"])
+	elements := map[string]string{
+		"H":  "Hydrogen",
+		"Li": "Lithium",
+	}
+	fmt.Println(elements["H"])
+	fmt.Println(elements["Li"])
+
+	emptyMap := map[string]float64{}
+	fmt.Println(emptyMap)
+}
