e7ffc91bafca1f757dd7038b74bc8149e912c0c5
Imil Ametov <charlz_klug@mail.ru>
2025-04-16 15:11:35 +0300

    Access struct field

--- 08/404_struct_many/main.go
+++ 08/404_struct_many/main.go
@@ -8,5 +8,11 @@ func main() {
 		word   string
 		toggle bool
 	}
+	myStruct.number = 3.14
+	myStruct.word = "pie"
+	myStruct.toggle = true
 	fmt.Printf("%#v\n", myStruct)
+	fmt.Println(myStruct.number)
+	fmt.Println(myStruct.word)
+	fmt.Println(myStruct.toggle)
 }
