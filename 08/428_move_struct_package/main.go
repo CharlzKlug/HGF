61ff715d8a2032c525dad813bbbd887609b6a7bc
Imil Ametov <charlz_klug@mail.ru>
2025-04-17 15:19:34 +0300

    Initializing struct like map

--- 08/428_move_struct_package/main.go
+++ 08/428_move_struct_package/main.go
@@ -9,4 +9,8 @@ func main() {
 	var s magazine.Subscriber
 	s.Rate = 4.99
 	fmt.Println(s.Rate)
+	subscribr := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
+	fmt.Println("Name:", subscribr.Name)
+	fmt.Println("Rate:", subscribr.Rate)
+	fmt.Println("Active:", subscribr.Active)
 }
