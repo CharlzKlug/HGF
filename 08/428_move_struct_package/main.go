44f1dbb6d4b3c4df54daf804823f671f6988c514
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-04-17 23:12:49 +0300

    Employee struct

--- 08/428_move_struct_package/main.go
+++ 08/428_move_struct_package/main.go
@@ -13,4 +13,10 @@ func main() {
 	fmt.Println("Name:", subscribr.Name)
 	fmt.Println("Rate:", subscribr.Rate)
 	fmt.Println("Active:", subscribr.Active)
+
+	var employee magazine.Employee
+	employee.Name = "Joy Carr"
+	employee.Salary = 60000
+	fmt.Println(employee.Name)
+	fmt.Println(employee.Salary)
 }
