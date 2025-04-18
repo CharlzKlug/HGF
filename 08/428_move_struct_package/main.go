c6968b2c07d89182123439ce917c18bcbd1e34a6
Imil Ametov <charlz_klug@mail.ru>
2025-04-18 12:09:15 +0300

    Setting struct with another struct

--- 08/428_move_struct_package/main.go
+++ 08/428_move_struct_package/main.go
@@ -6,24 +6,27 @@ import (
 )
 
 func main() {
-	var s magazine.Subscriber
-	s.Rate = 4.99
-	fmt.Println(s.Rate)
-	subscribr := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
-	fmt.Println("Name:", subscribr.Name)
-	fmt.Println("Rate:", subscribr.Rate)
-	fmt.Println("Active:", subscribr.Active)
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.HomeAddress.Street = "123 Oak St"
+	subscriber.HomeAddress.City = "Omaha"
+	subscriber.HomeAddress.State = "NE"
+	subscriber.HomeAddress.PostalCode = "68111"
 
-	var employee magazine.Employee
-	employee.Name = "Joy Carr"
-	employee.Salary = 60000
-	fmt.Println(employee.Name)
-	fmt.Println(employee.Salary)
+	fmt.Println("Subscriber Name:", subscriber.Name)
+	fmt.Println("Street:", subscriber.HomeAddress.Street)
+	fmt.Println("City:", subscriber.HomeAddress.City)
+	fmt.Println("State:", subscriber.HomeAddress.State)
+	fmt.Println("Postal Code:", subscriber.HomeAddress.PostalCode)
 
-	var address magazine.Address
-	address.Street = "123 Oak St"
-	address.City = "Omaha"
-	address.State = "NE"
-	address.PostalCode = "68111"
-	fmt.Println(address)
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.HomeAddress.Street = "456 Elm St"
+	employee.HomeAddress.City = "Portland"
+	employee.HomeAddress.State = "OR"
+	employee.HomeAddress.PostalCode = "97222"
+
+	fmt.Println("Employee Name:", employee.Name)
+	fmt.Println("Street:", employee.HomeAddress.Street)
+	fmt.Println("City:", employee.HomeAddress.City)
+	fmt.Println("State:", employee.HomeAddress.State)
+	fmt.Println("Postal Code:", employee.HomeAddress.PostalCode)
 }
