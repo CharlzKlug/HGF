801c0e498ac136f8a7a6f9c6d69d9f4316156edf
Imil Ametov <charlz_klug@mail.ru>
2025-04-25 10:06:56 +0300

    Adding validation to the setter methods

--- 10/498_setter_methods/main.go
+++ 10/498_setter_methods/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 type Date struct {
 	Year  int
@@ -8,22 +12,43 @@ type Date struct {
 	Day   int
 }
 
-func (d *Date) SetYear(year int) {
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
 	d.Year = year
+	return nil
 }
 
-func (d *Date) SetMonth(month int) {
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
 	d.Month = month
+	return nil
 }
 
-func (d *Date) SetDay(day int) {
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return fmt.Errorf("invalid day %d", day)
+	}
 	d.Day = day
+	return nil
 }
 
 func main() {
 	date := Date{}
-	date.SetYear(0)
-	date.SetMonth(14)
-	date.SetDay(50)
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(27)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(date)
 }
