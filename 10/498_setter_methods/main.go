bceb94d5645b07390d37734336af480f747145ef
Imil Ametov <charlz_klug@mail.ru>
2025-04-25 09:32:14 +0300

    Adding the remaining setter methods

--- 10/498_setter_methods/main.go
+++ 10/498_setter_methods/main.go
@@ -3,17 +3,27 @@ package main
 import "fmt"
 
 type Date struct {
-	Year int
+	Year  int
 	Month int
-	Day int
+	Day   int
 }
 
 func (d *Date) SetYear(year int) {
 	d.Year = year
 }
 
+func (d *Date) SetMonth(month int) {
+	d.Month = month
+}
+
+func (d *Date) SetDay(day int) {
+	d.Day = day
+}
+
 func main() {
 	date := Date{}
-	date.SetYear(2019)
-	fmt.Println(date.Year)
+	date.SetYear(0)
+	date.SetMonth(14)
+	date.SetDay(50)
+	fmt.Println(date)
 }
