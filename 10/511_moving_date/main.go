b5c003fe9fa310995a2211a75214143ffbd281fe
Imil Ametov <charlz_klug@mail.ru>
2025-04-25 15:52:37 +0300

    Accessing unexported fields through exported methods

--- 10/511_moving_date/main.go
+++ 10/511_moving_date/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"511_moving_date/calendar"
+	"fmt"
+	"log"
 )
 
 func main() {
 	date := calendar.Date{}
-	date.year = 2019
-	date.month = 14
-	date.day = 50
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(27)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(date)
 
-	date = calendar.Date{year: 0, month: 0, day: -2}
+	err = date.SetYear(0)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(date)
 }
