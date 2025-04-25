d151b75904a33955904ba2a7048fc76e31762136
Imil Ametov <charlz_klug@mail.ru>
2025-04-25 09:40:29 +0300

    Exercise

---
+++ 10/504_exercise/go.mod
@@ -0,0 +1,3 @@
+module 504_exercise
+
+go 1.24.1
---
+++ 10/504_exercise/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"504_exercise/geo"
+)
+
+func main() {
+	coordinates := geo.Coordinates{}
+	coordinates.SetLatitude(37.42)
+	coordinates.SetLongitude(-122.08)
+	fmt.Println(coordinates)
+}
